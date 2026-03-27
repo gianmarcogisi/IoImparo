39f0ac0c5c59136f67996e890306fab42af7dc9a
gianmarcogisi <gianmarcogisi25@gmail.com>
2026-03-27 10:57:27 +0100

    __init__.py

--- moduli/__init__.py
+++ moduli/__init__.py
@@ -1,8 +1,1 @@
-from supabase import create_client, Client
-import streamlit as st
 
-# Inizializza qui Supabase
-supabase: Client = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
-
-def db_salva_appunto(...): # incolla qui
-def db_get_miei_appunti(...): # incolla qui
