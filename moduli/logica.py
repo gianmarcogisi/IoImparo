ba9b1ac5e11c85efc4b5a5465dcae3bdc5ad4fa1
gianmarcogisi <gianmarcogisi25@gmail.com>
2026-03-27 10:53:11 +0100

    logica.py

---
+++ moduli/logica.py
@@ -0,0 +1,24 @@
+import st
+import time
+
+def gestisci_voto_esame(risposta_prof, st_session):
+    """Estrae il voto e calcola lo stato della sessione esame."""
+    voto = 0
+    try:
+        voto_str = risposta_prof.split("VOTO:")[1].strip()
+        voto = int("".join(filter(str.isdigit, voto_str[:3])))
+    except:
+        voto = 0
+    
+    if voto > 0 and voto < 18:
+        st_session.errori_totali += 1
+    
+    if st_session.errori_totali >= 4:
+        st_session.esame_bocciato = True
+        
+    return voto
+
+def calcola_esito_arena(is_host, sfida):
+    col_punti = "punteggio_host" if is_host else "punteggio_guest"
+    col_risposte = "risposte_host" if is_host else "risposte_guest"
+    return col_punti, col_risposte
